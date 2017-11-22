e4fe1fcd15d5e6564e9e8d3b1f7f6e15050fef7f
Matías Salinas <msalinas61192@gmail.com>
2017-11-22 17:02:05 -0300

    Response json

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
+var archivoJson = require('./archivo.json');
 var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  var data  = [ {'nombre' : 'Daniel' } ];
-  res.json(data);
+app.get('/personas', function(req, res) {
+  res.json(archivoJson);
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('servidor iniciado!');
 });
 
